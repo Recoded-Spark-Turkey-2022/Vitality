d2c31769c7dbee6a932eb3d428236f3fb537e1a6
LittleLebowski <o.f.atas@gmail.com>
2022-12-27 17:21:53 +0300

    Conselor pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
+
+import Home from './components/Home';
 import About from './components/About';
+import EnrollHome from './components/Counselor/EnrollHome';
+import Requirements from './components/Counselor/Requirements';
+import CreateAccount from './components/Counselor/CreateAccount';
+import ThankYou from './components/Counselor/ThankYou';
 
 function App() {
   return (
-    <div>
-     < About />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/enroll-home" element={<EnrollHome />} />
+      <Route path="/requirements" element={<Requirements />} />
+      <Route path="/createAccount" element={<CreateAccount />} />
+      <Route path="/thankYou" element={<ThankYou />} />
+    </Routes>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
-  
 );
