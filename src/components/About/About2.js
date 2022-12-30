e3a44ff31be5a22e47ba641db808d881557e6ee9
trteel1 <tateelamino@gmail.com>
2022-12-30 03:53:04 +0300

    About2 almost done

--- src/components/About/About2.js
+++ src/components/About/About2.js
@@ -1,61 +1,80 @@
 import React from "react";
-import Allan from "./About-imges/Rectangle.jpg"
+import Allan from "./About-imges/Allan.jpg"
+import Payam from "./About-imges/Payam.jpg"
+import Ahmed from "./About-imges/Ahmed.jpg"
+import Abdulmajeed from "./About-imges/Abdulmajeed.jpg"
+import Snoor from "./About-imges/Snoor.jpg"
+import Avraz from "./About-imges/Avraz.jpg"
 
 function About2() {
     return(
         <div >
-           <h1 className=" text-3xl mt-14 ml-12">WE ARE HEALING, NICE TO MEET YOU!</h1>
-           <h2 className=" text-[#424A4F] mt-16 ml-12">Meet the Team!</h2>
-           <div className=" flex flex-wrap flex-row">
-<div className=" h-28 w-32 ml-12 mt-14 -mr-32 ">
-           <div className="flex justify-center">
-  <div className="rounded-lg shadow-lg bg-blue-200 max-w-sm">
-    <a href="#!">
-      <img className="rounded-t-lg" src={Allan} alt="Allan Saleh"/>
-    </a>
-    <div >
-      <h5 className="text-gray-900 text-sm font-medium mb-2 mt-2 ml-6">Allan Saleh</h5>
-      <p className="text-gray-700 text-sm mb-4 mt-2 ml-3">Lead Engineer& Web Developer</p>
-     
+          <h1 className=" text-3xl mt-14 ml-12">WE ARE HEALING, NICE TO MEET YOU!</h1>
+           <h2 className=" text-[#424A4F] mt-16 ml-12 fw-bold">Meet the Team!</h2>
+           <div className =" flex flex-row flex-wrap mt-10 ml-14 mr-14">
+  <div className ="col">
+    <div className ="card h-14 w-32  items-stretch">
+      <img src={Allan} className ="card-img-top" alt="..." />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title fw-bold text-xs text-center">Allan Saleh</h5>
+        <p className ="card-text text-xs mt-4 text-center">Lead Engineer & Web Developer</p>
+      </div>
     </div>
+  </div>
+  <div className ="col ">
+    <div className ="card h-14 w-32 ">
+      <img src={Payam} className ="card-img-top" alt="..." />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title text-xs fw-bold text-center">Payam Abubakr</h5>
+        <p className ="card-text text-xs mt-4 text-center">Junior Designer & FE Developer</p>
+      </div>
     </div>
-     </div>
-           </div>
-           <div className=" ml-1 ">
-           <div className=" h-28 w-32 ml-36 mt-14">
-           <div className="flex justify-center">
-  <div className="rounded-lg shadow-lg bg-blue-200 max-w-sm">
-    <a href="#!">
-      <img className="rounded-t-lg" src={Allan} alt="Allan Saleh"/>
-    </a>
-    <div >
-      <h5 className="text-gray-900 text-sm font-medium mb-2 mt-2 ml-6">Allan Saleh</h5>
-      <p className="text-gray-700 text-sm mb-4 mt-2 ml-3">Lead Engineer& Web Developer</p>
-     
+  </div>
+  <div className ="col">
+    <div className ="card h-14 w-32 ">
+      <img src={Ahmed} className ="card-img-top" alt="Ahmed Mohammed" />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title fw-bold text-xs text-center">Ahmed Mohammed</h5>
+        <p className ="card-text text-xs mt-4 text-center">Junior Designer & FE Developer</p>
+      </div>
     </div>
+  </div>
+  <div className ="col">
+    <div className ="card h-14 w-32 ">
+      <img src={Abdulmajeed} className ="card-img-top" alt="..." />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title fw-bold text-xs text-center">Abdulmajeed Jaafer</h5>
+        <p className ="card-text text-xs mt-4 text-center">Lead Engineer & Web Developer</p>
+      </div>
     </div>
-     </div>
-           </div>
-           </div>
-           
+  </div>
+  <div className ="col">
+    <div className ="card h-14 w-32  ">
+      <img src={Snoor} className ="card-img-top" alt="Snoor Madih" />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title fw-bold text-xs text-center">Snoor Madih</h5>
+        <p className ="card-text text-xs mt-4 text-center">Junior Designer & FE Developer</p>
+      </div>
+    </div>
+  </div>
+  <div className ="col">
+    <div className ="card h-14 w-32 ">
+      <img src={Avraz} className ="card-img-top" alt="Avraz" />
+      <div className=" bg-green-800 m-0 h-1"><h1 className=" text-green-800">m</h1></div>
+      <div className ="card-body bg-cyan-100">
+        <h5 className ="card-title fw-bold text-xs text-center">Avraz Zebary</h5>
+        <p className ="card-text text-xs mt-4 text-center">Junior Designer & FE Developer</p>
+      </div>
+    </div>
+  </div>
+</div>
 
-           <div>
-           <div className=" h-28 w-32  -mr-32 mt-14 ml-5">
-           <div className="flex justify-center">
-  <div className="rounded-lg shadow-lg bg-blue-200 max-w-sm">
-    <a href="#!">
-      <img className="rounded-t-lg" src={Allan} alt="Allan Saleh"/>
-    </a>
-    <div >
-      <h5 className="text-gray-900 text-sm font-medium mb-2 mt-2 ml-6">Allan Saleh</h5>
-      <p className="text-gray-700 text-sm mb-4 mt-2 ml-3">Lead Engineer& Web Developer</p>
-     
-    </div>
-    </div>
-    </div>
-     </div>
-           </div>
-        </div>
+
         </div>
     )
 }
