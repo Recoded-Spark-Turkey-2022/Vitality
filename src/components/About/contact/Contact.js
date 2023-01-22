4fad66d179f55713a17202b97afa755e7e271c6c
trteel1 <tateelamino@gmail.com>
2023-01-22 22:42:53 +0300

    some changes

--- src/components/About/contact/Contact.js
+++ src/components/About/contact/Contact.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Group from "./Group-147.jpg"
+import './Contact.css'
 
 function Contact (){
     return(
         
         <div>
-            <h1 className=" text-3xl ml-20 mb-2 mt-9">SEND US YOUR REQUEST!</h1>
-            <p className=" ml-20 text-[#00000080] text-xl mr-24">Do you have a question, concern, idea, feedback, or problem?  If you need assistance, please fill out the form below and we&apos;d be happy to help!</p>
+            <div className="text">
+            <h1 className=" text-3xl  mb-2 mt-9">SEND US YOUR REQUEST!</h1>
+            <p className=" text-[#00000080] text-xl mr-24">Do you have a question, concern, idea, feedback, or problem?  If you need assistance, please fill out the form below and we&apos;d be happy to help!</p>
+            </div>
             <div className=" flex flex-row flex-wrap mb-20">
-                <div className="ml-20">
+                <div className=" radio">
                     <h1 className=" my-14 mb-8 text-lg">Type of contact</h1>
                     <input type="radio" name="type_of_contact" value="service" className=" mb-6"/> I have a question about the service.<br />
                     <input type="radio" name="type_of_contact" value="registered_client" className=" mb-6"/>  I&apos;m a registered client and I need support.<br />
@@ -19,22 +22,22 @@ function Contact (){
                     <input type="radio" name="type_of_contact" value="billing" className=" mb-6"/> I have a billing related question.<br />
                 </div>
                 <div>
-                    <img src={Group} alt='Healling' className=" w-96 mt-16 ml-96 h-80 "/>
+                    <img src={Group} alt='Healling' className=" photo"/>
                 </div>
             </div>
-            <div className="flex flex-row flex-wrap  mb-20">
-                <div className=" w-3/4 ml-20  ">
+            <div className=" contener">
+                <div className=" first  ">
                      <h5 className=" -mb-2">Full Name:  </h5><br/> 
-                     <input type="Name" id="Name" name="Name"  placeholder="Enter your full name here..." className="shadow p-3 mb-5 bg-white rounded w-1/3 h-20"/> <br/>
+                     <input type="Name" id="Name" name="Name"  placeholder="Enter your full name here..." className="box"/> <br/>
                      <h5 className=" -mb-2 -mt-2">Email:   </h5><br/>
-                     <input type="Email" id="Email" name="Email"  placeholder="Enter your email address here..." className="shadow p-3 mb-5 bg-white rounded h-20 w-1/3"/> <br/>
+                     <input type="Email" id="Email" name="Email"  placeholder="Enter your email address here..." className="box"/> <br/>
                      <h5 className=" -mb-2 -mt-2">Details:  </h5><br/>
-                     <textarea id="Details" name="Details" placeholder="Enter your details here..." className="shadow p-3 mb-5 bg-white rounded  w-1/3 h-44"/> <br/>
+                     <textarea id="Details" name="Details" placeholder="Enter your details here..." className="box"/> <br/>
                      <button type="submit" className="btn btn btn-info bg-[#2DD3E3] -mt-8 w-56">Submit</button>
                 </div>
-                <div className=" d-table-cell mt-44 -ml-96 ">
-                    <div className="card w-96  h-52 bg-[#EAF8F9] border border-0" >
-                    <div className="card-body">
+                <div className=" second ">
+                    <div className="card  bg-[#EAF8F9] border border-0" >
+                    <div className="card-body  ">
                     <h1 className=" text-lg mb-3 -mt-1 ml-11">Find Us At:</h1><br/>
                     <p className="text-[#0000008A] -mb-6 ml-11">Nergiz Plaza</p><br/>
                     <p className="text-[#0000008A] -mb-6 ml-11">3rd Floor</p><br/>
