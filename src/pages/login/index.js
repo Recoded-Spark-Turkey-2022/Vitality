f7d700cf2c23961b3c025e8e7849d84ea246da9a
Seda Unal <sedaylmz06@outlook.com>
2023-01-24 23:03:29 +0300

    view profile is not done completely

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,77 +1,88 @@
 /* eslint-disable no-console */
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
-import { signInWithEmailAndPassword,
-        onAuthStateChanged} from 'firebase/auth';
-import {auth} from "../../config/fire"
+import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../config/fire';
 
-import  Logo  from '../../assets/images/login-image.png';
+import Logo from '../../assets/images/login-image.png';
 import './login.css';
 
-
-
 function Login() {
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
+  const [loginEmail, setLoginEmail] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
 
-  const[setUser] = useState({});
+  const [setUser] = useState({});
   onAuthStateChanged(auth, (currentUser) => {
     setUser(currentUser);
-  })
+  });
   const login = async () => {
     try {
-      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword );
-      console.log(user)
-       }catch (error) {
-         console.log(error.message);
-       }
-  }
-  
+      console.log('login');
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+      localStorage.setItem('user', JSON.stringify(user.user));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div className="container">
-      <div className='mb-5'>
+      <div className="mb-5">
         <h1>LOGIN</h1>
-        
-        <div className='mb-5'>
-        <h4>User logged In</h4>
-        
-      </div>
-      </div>
-  <div className="row">
-    <div className="col">
-    <Card>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        
-        <Form.Control type="email" placeholder="Your Email"  onChange={(event) => {setLoginEmail(event.target.value)}} />
-        
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        
-        <Form.Control type="password" placeholder=" Your Password" onChange={(event) => {setLoginPassword(event.target.value)}} />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit" onClick={login} >
-       Login
-      </Button>
-      <Link to="/sign-up"><Button variant="secondary" type="submit">
-      Signup 
-      </Button></Link>
-    </Form>
-    </Card>
-    </div>
-    <div className="col">
-    <img src={Logo} alt ="login" />
-    </div>
-  </div>
-  
-  </div>
+        <div className="mb-5">
+          <h4>User logged In</h4>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <Card>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  type="email"
+                  placeholder="Your Email"
+                  onChange={(event) => {
+                    setLoginEmail(event.target.value);
+                  }}
+                />
+              </Form.Group>
 
-  )
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  placeholder=" Your Password"
+                  onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="button" onClick={()=>login()}>
+                Login
+              </Button>
+              <Link to="/sign-up">
+                <Button variant="secondary" type="submit">
+                  Signup
+                </Button>
+              </Link>
+            </Form>
+          </Card>
+        </div>
+        <div className="col">
+          <img src={Logo} alt="login" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
