943c8964450cd4856b766f773e0428ba63d47e59
trteel1 <tateelamino@gmail.com>
2023-01-25 14:10:19 +0300

    some changes

--- src/components/Home/ContactCard/ContactCard.js
+++ src/components/Home/ContactCard/ContactCard.js
@@ -7,30 +7,30 @@ import './ContactCard.css'
 function ContactCard (){
     return(
     <div> 
-        <h1>we can communicate through</h1>
+        <h1 className="header1">we can communicate through</h1>
         <div className="card_cont">
              <div className="card">
           <img src={Call} alt="Avatar" className="img1" />
              <div className="container">
               
-                 <h2><b>Voice Call</b></h2>
-                  <p>Feeling ready to start a conversation? Give your therapist a voice call and talk your heart out!</p>
+                 <h2 className="header"><b>Voice Call</b></h2>
+                  <p className="parag">Feeling ready to start a conversation? Give your therapist a voice call and talk your heart out!</p>
               </div>
         </div>
         <div className="card">
           <img src={Chat} alt="Avatar" className="img1" />
              <div className="container">
               
-                 <h2><b>Chat</b></h2>
-                  <p>Need to talk to someone?Come have a chat with your therapist!</p>
+                 <h2 className="header"><b>Chat</b></h2>
+                  <p className="parag">Need to talk to someone?Come have a chat with your therapist!</p>
               </div>
         </div>
         <div className="card">
           <img src={video} alt="Avatar" className="img1" />
              <div className="container">
               
-                 <h2><b>Video Call</b></h2>
-                  <p>For better experience therapists recommend video calls, but always remember that its a choice!</p>
+                 <h2 className="header"><b>Video Call</b></h2>
+                  <p className="parag">For better experience therapists recommend video calls, but always remember that its a choice!</p>
               </div>
         </div>
         </div>
