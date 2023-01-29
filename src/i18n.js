a7b69758235fc4954bd5eb81794351fec013727e
LittleLebowski <o.f.atas@gmail.com>
2023-01-29 20:08:14 +0300

    removed unnecessary file

--- src/i18n.js
+++ src/i18n.js
@@ -2,11 +2,9 @@ import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// the translations
 import translationEN from './localisation/en/translation.json';
 import translationTR from './localisation/tr/translation.json';
 
-// the translations
 const resources = {
   en: {
     translation: translationEN,
@@ -24,13 +22,12 @@ i18next
     resources,
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false, // react already safes from xss
+      escapeValue: false,
     },
 
     supportedLngs: ['en', 'tr'],
     debug: false,
 
-    // Options for language detector
     detection: {
       order: ['path', 'cookie', 'htmlTag'],
       caches: ['cookie'],
