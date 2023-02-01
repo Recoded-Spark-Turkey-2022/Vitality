c5dbf792e71aa969b04b90713b50f99c9f296259
Seda Unal <sedaylmz06@outlook.com>
2023-02-01 21:41:39 +0300

    changed firebase config

--- src/config/fire.js
+++ src/config/fire.js
@@ -11,14 +11,13 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCASciJjdWKvdT-SVpgRKCXzwOoQ4EKROo',
-  authDomain: 'vitality-43d63.firebaseapp.com',
-  databaseURL: 'https://vitality-43d63-default-rtdb.firebaseio.com',
-  projectId: 'vitality-43d63',
-  storageBucket: 'vitality-43d63.appspot.com',
-  messagingSenderId: '854106165892',
-  appId: '1:854106165892:web:bc4054e7ebeb77e0a8a8d1',
-  measurementId: 'G-JXHY9S4R5V',
+  apiKey: "AIzaSyA5XLLxEmDUXxyW_LGr6pNhL-nlwlu4sN4",
+  authDomain: "vitality-demo.firebaseapp.com",
+  projectId: "vitality-demo",
+  storageBucket: "vitality-demo.appspot.com",
+  messagingSenderId: "197888380495",
+  appId: "1:197888380495:web:4e3328098f18129829643a",
+  measurementId: "G-305YHDSLHN"
 };
 
 const app = initializeApp(firebaseConfig);
